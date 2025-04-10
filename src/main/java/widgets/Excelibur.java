e6eb7ec5129201922f5e9bb9d7aca8d1e83b482c
Kilian Glomb <kglomb@2orgu.com>
2025-04-10 09:26:52 +0200

    - translations changes

--- src/main/java/widgets/Excelibur.java
+++ src/main/java/widgets/Excelibur.java
@@ -386,7 +386,7 @@ public class Excelibur extends JPanel implements OnLocaleMissing, OnBrandMissing
 
     void openExportDialog() {
 
-        String[] options = {"Export as Json", "Export as new Excel File", "Fill missing in Excel File"};
+        String[] options = {"Export as Json", "Export as Excel File", "Merge into Excel File"};
         JOptionPane pane = new JOptionPane();
         pane.setPreferredSize(new Dimension(800, 600));
         int selection = pane.showOptionDialog(this, "How would you like to export the data?", "Export",
